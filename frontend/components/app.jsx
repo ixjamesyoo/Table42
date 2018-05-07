ab716808ad2ec07ca147d25ce36d34d74fdedd27
James Yoo <jameshkyoo@gmail.com>
2018-05-07 17:01:46 -0400

    create root/app components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Table42</h1>
+  </div>
+);
+
+export default App;
--- frontend/table42.jsx
+++ frontend/table42.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
-const store = configureStore();
-window.getState = store.getState;
-window.dispatch = store.dispatch;
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // test start
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // test end
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Table42</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
