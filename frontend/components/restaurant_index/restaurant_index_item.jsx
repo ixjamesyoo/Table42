fe6c7351f71e053ace98839ab2f52873a0455095
James Y <jameshkyoo@gmail.com>
2018-05-17 12:38:44 -0400

    finsh styling reviews, res index/show pages

--- frontend/components/restaurant_index/restaurant_index_item.jsx
+++ frontend/components/restaurant_index/restaurant_index_item.jsx
@@ -16,20 +16,19 @@ const RestaurantIndexItem = ({ restaurant }) => {
         <Link to={ `/restaurants/${restaurant.id}` }><h3
           className="restaurant-name">{ restaurant.name }</h3></Link>
 
-        <ul>
+        <ul className="res-ul">
           <li className="restaurant-overview">{ cuisineText }</li>
           <li>{ dollarSigns(restaurant.price_range) }</li>
           <li className="restaurant-overview">{ restaurant.address }</li>
         </ul>
 
-        <section className="search-result-reservation-section">
-          <button className="navbar-button demo-button">BUTTON</button>
-          <button className="navbar-button demo-button">BUTTON</button>
-          <button className="navbar-button demo-button">BUTTON</button>
-          <button className="navbar-button demo-button">BUTTON</button>
-          <button className="navbar-button demo-button">BUTTON</button>
-          <button className="navbar-button demo-button">BUTTON</button>
-        </section>
+        <ul className="other-info">
+          <p id="index-overall">{`Overall Rating: ${ restaurant.overall_rating }` }</p>
+          <p id="index-recommend">
+            <i className="fa fa-thumbs-up" aria-hidden="true"/>
+            {`${Math.round(restaurant.recommendation_rate * 100)}% of diners recommend this establishment.`}
+          </p>
+        </ul>
       </section>
 
     </div>
