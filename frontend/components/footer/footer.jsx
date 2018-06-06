ad5127edfab49f310eb6a92ae8ac260e72672492
James Y <jameshkyoo@gmail.com>
2018-06-06 13:24:12 -0400

    add portfolio link to footer, remove readme link (it's in the repo after all)

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,9 +8,9 @@ export default () => {
           <p>Table42 was built by James Yoo using React/Redux for the frontend and Ruby on Rails/PostgreSQL for the backend. It was inspired by the web application OpenTable.</p>
         </div>
         <ul className="site-footer-links">
+          <li><a href="http://www.james-yoo.com/" target="_blank"><i className="fa fa-laptop"></i><span>Portfolio</span></a></li>
           <li><a href="https://github.com/ixjamesyoo" target="_blank"><i className="fab fa-github"></i><span>Github</span></a></li>
           <li><a href="https://www.linkedin.com/in/jameshkyoo/" target="_blank"><i className="fab fa-linkedin"></i><span>LinkedIn</span></a></li>
-          <li><a href="https://github.com/ixjamesyoo/Table42" target="_blank"><i className="far fa-file-alt" target="_blank"></i><span>README</span></a></li>
         </ul>
       </div>
     </footer>
